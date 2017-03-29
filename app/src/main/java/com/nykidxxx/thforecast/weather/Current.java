1af3d89808021a2cfbc58bb56abb689160bdaa9a
Nykidxxx <yao.ni@yahoo.com>
2017-03-29 18:53:58 -0400

    Initial commit

---
+++ app/src/main/java/com/nykidxxx/thforecast/weather/Current.java
@@ -0,0 +1,126 @@
+package com.nykidxxx.thforecast.weather;
+// Created on 3/29/2017
+
+
+import com.nykidxxx.thforecast.R;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+public class Current {
+    private String mIcon;
+    private long mTime;
+    private double mTemperature;
+    private double mHumidity;
+    private double mPrecipChance;
+    private String mSummary;
+    private String mTimeZone;
+
+    public String getTimeZone() {
+        return mTimeZone;
+    }
+
+    public void setTimeZone(String timeZone) {
+        mTimeZone = timeZone;
+    }
+
+    public String getIcon() {
+        return mIcon;
+    }
+
+    public void setIcon(String icon) {
+        mIcon = icon;
+    }
+
+    public int getIconId(){
+        //clear-day, clear-night, rain, snow, sleet, wind, fog, cloudy, partly-cloudy-day, or partly-cloudy-night
+        int iconId = R.mipmap.clear_day;
+
+        if(mIcon.equals("clear-day")){
+            iconId = R.mipmap.clear_day;
+        }
+        else if (mIcon.equals("clear-night")){
+            iconId = R.mipmap.clear_night;
+        }
+        else if (mIcon.equals("rain")) {
+            iconId = R.mipmap.rain;
+        }
+        else if (mIcon.equals("snow")) {
+            iconId = R.mipmap.snow;
+        }
+        else if (mIcon.equals("sleet")) {
+            iconId = R.mipmap.sleet;
+        }
+        else if (mIcon.equals("wind")) {
+            iconId = R.mipmap.wind;
+        }
+        else if (mIcon.equals("fog")) {
+            iconId = R.mipmap.fog;
+        }
+        else if (mIcon.equals("cloudy")) {
+            iconId = R.mipmap.cloudy;
+        }
+        else if (mIcon.equals("partly-cloudy-day")) {
+            iconId = R.mipmap.partly_cloudy;
+        }
+        else if (mIcon.equals("partly-cloudy-night")) {
+            iconId = R.mipmap.cloudy_night;
+        }
+
+        return iconId;
+    }
+
+    public long getTime() {
+        return mTime;
+    }
+
+    public void setTime(long time) {
+        mTime = time;
+    }
+
+    public String getFormattedTime(){
+        //Get time
+        Date dateTime = new Date(getTime() * 1000);
+        //Dictate how we want it formatted
+        SimpleDateFormat mFormat = new SimpleDateFormat("h:mm a");
+        mFormat.setTimeZone(TimeZone.getTimeZone(getTimeZone()));
+        //Apply our desired format to the current time
+        String timeString = mFormat.format(dateTime);
+
+        return timeString;
+    }
+
+    public int getTemperature() {
+        return (int)Math.round(mTemperature);
+    }
+
+    public void setTemperature(double temperature) {
+        mTemperature = temperature;
+    }
+
+    public double getHumidity() {
+        return mHumidity;
+    }
+
+    public void setHumidity(double humidity) {
+        mHumidity = humidity;
+    }
+
+    public int getPrecipChance() {
+        double precipPercentage = mPrecipChance * 100;
+        return (int)Math.round(precipPercentage);
+    }
+
+    public void setPrecipChance(double precipChance) {
+        mPrecipChance = precipChance;
+    }
+
+    public String getSummary() {
+        return mSummary;
+    }
+
+    public void setSummary(String summary) {
+        mSummary = summary;
+    }
+}
